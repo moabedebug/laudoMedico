3dad995a7983b33055f35dfd9fbc3ee67e02bd6b
Moabe <moabeluis2018@gmail.com>
2025-05-12 12:41:18 -0300

    feat: reorganize imports and add error handling in the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import cors from 'cors';
+
+dotenv.config();
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("Erro no processamento da requisição:", err);
+  res.status(500).json({ message: "Erro interno do servidor" });
+});
