51ce009977e0dbac7a4ebcc7f63fa4099bfc9e99
Moabe <moabeluis2018@gmail.com>
2025-05-12 12:41:34 -0300

    feat: refactor server startup to ensure connection to MongoDB before starting

--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ import { env } from "./config/env.js";
 
 const PORT = env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Server iniciado: http://localhost:${PORT}`);
-    connectDB();
-})
+async function startServer() {
+    try {
+        await connectDB();
+        console.log("Conexão com o MongoDB estabelecida.");
+
+        app.listen(PORT, () => {
+            console.log(`Server iniciado: http://localhost:${PORT}`);
+        })
+    } catch (err) {
+        console.error("Erro ao conectar com o banco de dados:", err.message);
+        process.exit(1);
+    }
+}
+
+startServer();
