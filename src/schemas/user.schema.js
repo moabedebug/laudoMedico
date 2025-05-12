2f990eba458dee5df21798793d241e98828fdf2f
Moabe <moabeluis2018@gmail.com>
2025-05-12 12:32:16 -0300

    feat: add trim to signup schema validations for name, email and password field

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
-    name: z.string().min(1, "Nome é obrigatório"),
-    email: z.string().email("Email inválido"),
-    passwor: z.string().min(4, "Senha deve conter no mínimo 4 caracteres")
+    name: z.string().trim().min(1, "Nome é obrigatório"),
+    email: z.string().trim().email("Email inválido"),
+    password: z.string().trim().min(4, "Senha deve conter no mínimo 4 caracteres")
 });
