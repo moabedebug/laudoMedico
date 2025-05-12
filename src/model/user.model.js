cec1d8530531fa018c936cfcc718fa5a2db0db7b
Moabe <moabeluis2018@gmail.com>
2025-05-12 12:04:32 -0300

    feat: aprimorar o esquema do usuário com validações adicionais e transformação JSON

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -4,16 +4,27 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     passwordHash: {
         type: String,
         required: true,
     }
+}, { timestamps: true });
+
+userSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    delete ret.passwordHash;
+    delete ret.__v;
+    return ret;
+  }
 });
 
 export const User = mongoose.model('User', userSchema);
