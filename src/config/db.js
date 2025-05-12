c7a274971209fb07d95cf6d41dff8de0f605cbca
Moabe <moabeluis2018@gmail.com>
2025-05-12 10:35:29 -0300

    chore: add MongoDB connection function in db.js

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { ENV_VARS } from "./envVars";
+
+export const connectDB = async () => {
+    try{
+        const con = await mongoose.connect(ENV_VARS.MONGO_URI)
+        console.log("MongoDB conectado:" + con.connection.host)
+    } catch (err) {
+        console.log("Erro de conexão do MongoDB:" + err.massage)
+        process.exit(1)
+    }
+}
