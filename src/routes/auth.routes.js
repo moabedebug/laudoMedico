f04f29f0a261b02fa6179b9825bd5ee0745bacf0
Moabe <moabeluis2018@gmail.com>
2025-05-12 15:14:46 -0300

    feat: add login route with validation schema to authentication routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { login, signup } from '../controller/auth.controller.js';
 import { validateSchema } from '../middlewares/validateSchema.js';
-import { signupSchema } from '../schemas/user.schema.js';
+import { signupSchema, loginSchema } from '../schemas/user.schema.js';
 
 const router = express.Router();
 
 router.post('/signup', validateSchema(signupSchema), signup);
+router.post('/login',validateSchema(loginSchema), login)
 
 router.use((err, req, res, next) => {
   console.error("Erro no processamento da requisição:", err);
