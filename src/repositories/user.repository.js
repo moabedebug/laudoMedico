1b5e2e0b5b72e6d172032106e6a1faef9cb59af0
Moabe <moabeluis2018@gmail.com>
2025-05-12 15:15:01 -0300

    feat: remove findByName method from UserRepository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -2,6 +2,5 @@ import { User } from '../model/user.model.js'
 
 export const UserRepository = {
     findByEmail: (email) => User.findOne({ email }),
-    findByName: (name) => User.findOne({ name }),
     create: (userData) => new User(userData).save()
 };
