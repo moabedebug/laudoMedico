13c8bc89d8445b71480f269d3e4578d1c11aaf75
Moabe <moabeluis2018@gmail.com>
2025-05-13 11:55:06 -0300

    fix: fix validation middleware import to use correct validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { authenticate } from '../middlewares/auth.middleware.js'
-import { validateAuthSchema } from '../middlewares/validateSchema.js'
+import { validateSchema } from '../middlewares/validateSchema.js'
 import { signupSchema, loginSchema } from '../schemas/auth.schema.js'
 import { login, logout, signup } from '../controller/auth.controller.js'
 
 const router = express.Router()
 
-router.post('/signup', validateAuthSchema(signupSchema), signup)
-router.post('/login', validateAuthSchema(loginSchema), login)
+router.post('/signup', validateSchema(signupSchema), signup)
+router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', authenticate, logout)
 
 router.use((err, req, res, _next) => {
