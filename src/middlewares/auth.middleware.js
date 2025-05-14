77487e047ab821551c3b4c60df60dae873a8eaaa
Moabe <moabeluis2018@gmail.com>
2025-05-14 13:13:39 -0300

    feat: refactor authentication middleware to use centralized error module

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,25 +1,25 @@
 import jwt from 'jsonwebtoken'
 import { env } from '../config/env.js'
 
-import UnauthorizedError from '../services/errors/UnauthorizedError.js'
+import * as Errors from '../services/errors/index.js'
 
 export function auth(req, res, next) {
   const token = req.cookies.token
 
   if (!token) {
-    return next(new UnauthorizedError('Token não encontrado.'))
+    return next(new Errors.UnauthorizedError('Usuário não autenticado.'))
   }
 
   try {
     const decoded = jwt.verify(token, env.JWT_SECRET)
 
     if (!decoded || !decoded.id) {
-      return next(new UnauthorizedError('Token inválido.'))
+      return next(new Errors.UnauthorizedError('Token inválido.'))
     }
 
     req.user = decoded
     next()
   } catch (err) {
-    return next(new UnauthorizedError('Token inválido ou expirado.'))
+    return next(new Errors.UnauthorizedError('Token não recebido'))
   }
 }
