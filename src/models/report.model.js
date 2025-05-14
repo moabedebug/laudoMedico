88001187b9c31e2eb1fbaaa28c6b20c558d7e224
Moabe <moabeluis2018@gmail.com>
2025-05-14 15:24:40 -0300

    feat: add report template with Mongoose schema

---
+++ src/models/report.model.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+export const reportSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Patient',
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      require: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    observations: {
+      type: String,
+      trim: true,
+    },
+    recommendation: {
+      type: String,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true, validateBeforeSave: true },
+)
+
+reportSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    delete ret.__v
+    return ret
+  },
+})
+
+export const Report = mongoose.model('Reports', reportSchema)
